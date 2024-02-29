6c34af7002f16f8296436a308d71885a870cc9fb
angel <angelocean271201@gmail.com>
2024-02-29 17:32:43 -0600

    Ya tiene una segunda linea

--- clase1.java
+++ clase1.java
@@ -1,5 +1,6 @@
 public class clase1{
     public static void main(String[] args) {
     System.out.println("Esto es un comienzo");
+    System.out.println("Hola esta es una segunda linea");
     }
 }
